18ece53809a0e6f88ae0263785dbffe551d7f8fd
pcion <patrick.ciongoli@gmail.com>
2022-06-20 18:08:55 -0400

    updated profile links

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import PageContent from "../PageContent";
 import About from "../About";
+import Archives from "../Archives";
 import Portfolio from "../Portfolio";
 // import Contact from '../Contact';
-import Archives from "../Archives";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Page({ currentPage }) {
    const renderPage = () => {
       switch (currentPage.name) {
+         case "archives":
+            return <Archives />;
          case "about me":
             return <About />;
          case "portfolio":
             return <Portfolio />;
          // case 'contact':
          //   return <Contact />;
-         case "archives":
-            return <Archives />;
          default:
             return <About />;
       }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,54 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Project from "../Project";
 
 function Portfolio() {
+   // Replace links with deployed projects and GitHub repos
+   const [projects] = useState([
+      {
+         name: "surf-report",
+         description: "MERN Stack",
+         link: "https://github.com",
+         repo: "https://github.com",
+      },
+      {
+         name: "pastel-puzzels",
+         description: "MERN Stack",
+         link: "https://github.com",
+         repo: "https://github.com",
+      },
+      {
+         name: "run-buddy",
+         description: "HTML/CSS",
+         link: "https://github.com",
+         repo: "https://github.com",
+      },
+      {
+         name: "led-wall",
+         description: "Node/IoT",
+         link: "https://github.com",
+         repo: "https://github.com",
+      },
+      {
+         name: "calculator",
+         description: "React/JavaScript/CSS",
+         link: "https://github.com",
+         repo: "https://github.com",
+      },
+   ]);
 
-  // Replace links with deployed projects and GitHub repos
-  const [projects] = useState([
-    {
-      name: 'surf-report',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-    {
-      name: 'pastel-puzzels',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-    {
-      name: 'run-buddy',
-      description: 'HTML/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-    {
-      name: 'led-wall',
-      description: 'Node/IoT',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-    {
-      name: 'calculator',
-      description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-  ]);
-
-  return (
-    <div>
-      <div className="flex-row">
-        {projects.map((project, idx) => (
-          <Project
-            project={project}
-            key={"project" + idx}
-          />
-        ))}
+   return (
+      <div>
+         <div className="flex-row">
+            {projects.map((project, idx) => (
+               <Project project={project} key={"project" + idx} />
+            ))}
+         </div>
       </div>
-    </div>
-  );
-};
+   );
+}
 
 export default Portfolio;
