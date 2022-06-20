8780896af792a3cb3a459b435b1ad52959abf1ff
pcion <patrick.ciongoli@gmail.com>
2022-06-20 19:20:56 -0400

    Updates

---
+++ precache-manifest.0b02898ec0a86fa5352c77f9bb50e9af.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "161d91fe115ccab9c9ed",
+    "url": "/pciongoli/psychic-sniffle/static/css/main.dd7b7b1a.chunk.css"
+  },
+  {
+    "revision": "161d91fe115ccab9c9ed",
+    "url": "/pciongoli/psychic-sniffle/static/js/main.06d76a8a.chunk.js"
+  },
+  {
+    "revision": "2ab80f1cbf94b671cb21",
+    "url": "/pciongoli/psychic-sniffle/static/js/runtime~main.828dc0d2.js"
+  },
+  {
+    "revision": "853814d46d1e35ff7c15",
+    "url": "/pciongoli/psychic-sniffle/static/js/2.86c9b7d0.chunk.js"
+  },
+  {
+    "revision": "db9b7256f73e1acfef072f65052514a1",
+    "url": "/pciongoli/psychic-sniffle/static/media/cover-image.db9b7256.jpg"
+  },
+  {
+    "revision": "358f20e45c790517a9c198f2566211f0",
+    "url": "/pciongoli/psychic-sniffle/static/media/4.358f20e4.jpg"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/pciongoli/psychic-sniffle/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/pciongoli/psychic-sniffle/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/pciongoli/psychic-sniffle/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/pciongoli/psychic-sniffle/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/pciongoli/psychic-sniffle/static/media/surf-report.12557c9b.jpg"
+  },
+  {
+    "revision": "5b90a4e6431c7bc1a9b06789a2ec651b",
+    "url": "/pciongoli/psychic-sniffle/index.html"
+  }
+];
