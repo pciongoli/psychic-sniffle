500e399f392d0501fa80b8d1d53995aa9bae4c76
pcion <patrick.ciongoli@gmail.com>
2022-06-21 13:30:50 -0400

    updated styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,37 +1,31 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-  const {
-    pages = [],
-    setCurrentPage,
-    currentPage,
-  } = props;
+   const { pages = [], setCurrentPage, currentPage } = props;
 
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentPage.name);
-  }, [currentPage]);
+   useEffect(() => {
+      document.title = capitalizeFirstLetter(currentPage.name);
+   }, [currentPage]);
 
-  return (    
+   return (
       <nav>
-        <ul className="flex-row">
-          {pages.map((Page) => (
-            <li
-              className={`mx-5 ${
-                currentPage.name === Page.name && 'navActive'
-                }`}
-              key={Page.name}
-            >
-              <span
-                onClick={() => setCurrentPage(Page)}
-              >
-                {capitalizeFirstLetter(Page.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
+         <ul className="flex-row">
+            {pages.map((Page) => (
+               <li
+                  className={`mx-5 ${
+                     currentPage.name === Page.name && "navActive"
+                  }`}
+                  key={Page.name}
+               >
+                  <span onClick={() => setCurrentPage(Page)}>
+                     {capitalizeFirstLetter(Page.name)}
+                  </span>
+               </li>
+            ))}
+         </ul>
       </nav>
-  );
+   );
 }
 
 export default Nav;
